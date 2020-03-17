407ad9931de9d4023f6931d592e07e62c476553a
Yonchev <40342250@live.napier.ac.uk>
2020-03-17 21:20:04 +0000

    Implementing shaunting-yard

--- bin/main.dart
+++ bin/main.dart
@@ -1,7 +1,76 @@
-import "dart:collection";
+import 'dart:collection';
 
+import 'package:stack/stack.dart';
+import '../lib/operator.dart';
+import '../lib/constants.dart' as Constants;
+
+bool isNumeric(String s) {
+  if(s == null) {
+    return false;
+  }
+  return double.parse(s, (e) => null) != null;
+}
+
+bool isOperator(String token){
+  if(token == null){
+    return false;
+  }
+  return Constants.SUPPORTED_OPERATORS.contains(token);
+}
 
 void main(){
-  final Queue<String> tokens = new Queue<String>();
+  final String mathInfixExpression = "2 * ( 3 - 4 ) - 3";
 
+  final Queue<String> expressionTokens = new Queue<String>();
+  final Stack<Operator> operators = new Stack<Operator>();
+  final Queue<String> output = new Queue<String>();
+
+  // Load characters in expressionTokens Queue
+  for (var char in mathInfixExpression.split("")) {
+    expressionTokens.add(char);  
+  }
+
+  // Load tokens into operators and output
+  while (expressionTokens.isNotEmpty) {
+    String token =expressionTokens.removeFirst();
+
+    if(isNumeric(token)){
+      output.add(token);
+    }
+
+    else if(isOperator(token)){
+        Operator operator = new Operator(token);
+        Operator top_operator = null;
+        if(operators.isNotEmpty){
+          Operator top_operator = operators.top();
+        }
+        
+
+        if(token == Constants.CLOSING_BRACKET){
+          while(top_operator != null && top_operator != Constants.OPENING_BRACKET ){
+            output.add(top_operator.operator);
+            operators.pop();
+            top_operator = operators.top();
+          }
+          operators.push(operator);
+        }
+        else if(token ==Constants.OPENING_BRACKET){
+          operators.push(operator);
+        } else {
+          while(top_operator != null && top_operator.getPrecedence() > operator.getPrecedence()){
+            output.add(top_operator.operator);
+            operators.pop();
+            top_operator =operators.top();
+          }
+          operators.push(operator);
+        }
+    }
+  }
+  
+  while (operators.isNotEmpty) {
+        Operator operator = operators.pop();
+        output.add(operator.operator);
+  }
+
+  
 }
---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+const List<String> SUPPORTED_OPERATORS =["+", "-", "*", "/", "(", ")"];
+const String CLOSING_BRACKET = ")";
+const String OPENING_BRACKET = "(";
+
